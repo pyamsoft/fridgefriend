18dde413f079dbdd5dbc942f333977856c74cdfe
pyamsoft <developer.pyamsoft@gmail.com>
2019-06-15 20:25:05 -0700

    Remove noise

--- app/src/main/java/com/pyamsoft/fridge/locator/LocationUpdateReceiver.kt
+++ app/src/main/java/com/pyamsoft/fridge/locator/LocationUpdateReceiver.kt
@@ -19,13 +19,8 @@ package com.pyamsoft.fridge.locator
 
 import android.content.Context
 import com.pyamsoft.fridge.locator.map.GmsLocatorBroadcastReceiver
-import timber.log.Timber
 
 internal class LocationUpdateReceiver internal constructor() : GmsLocatorBroadcastReceiver() {
-
-  init {
-    Timber.d("Created new instance")
-  }
 
   override fun onInject(context: Context) {
   }
