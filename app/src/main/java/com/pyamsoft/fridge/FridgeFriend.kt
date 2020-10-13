9cf18bd5ff17edf50320445b2ecd5093f97c3b03
pyamsoft <developer.pyamsoft@gmail.com>
2020-10-12 20:38:36 -0700

    Clean up init

--- app/src/main/java/com/pyamsoft/fridge/FridgeFriend.kt
+++ app/src/main/java/com/pyamsoft/fridge/FridgeFriend.kt
@@ -68,14 +68,15 @@ class FridgeFriend : Application() {
                 provider.theming(),
                 provider.imageLoader(),
                 MainActivity::class.java
-            )
-            onInitialized()
+            ).apply {
+                onInitialized(this)
+            }
         }
     }
 
-    private fun onInitialized() {
+    private fun onInitialized(component: FridgeComponent) {
         addLibraries()
-        requireNotNull(component).inject(this)
+        component.inject(this)
         beginWork()
     }
 
@@ -91,70 +92,15 @@ class FridgeFriend : Application() {
         }
     }
 
-    private fun addLibraries() {
-        // We are using pydroid-notify
-        OssLibraries.usingNotify = true
-
-        OssLibraries.add(
-            "Room",
-            "https://android.googlesource.com/platform/frameworks/support/+/androidx-master-dev/room/",
-            "The AndroidX Jetpack Room library. Fluent SQLite database access."
-        )
-        OssLibraries.add(
-            "WorkManager",
-            "https://android.googlesource.com/platform/frameworks/support/+/androidx-master-dev/work/",
-            "The AndroidX Jetpack WorkManager library. Schedule periodic work in a device friendly way."
-        )
-        OssLibraries.add(
-            "Dagger",
-            "https://github.com/google/dagger",
-            "A fast dependency injector for Android and Java."
-        )
-        OssLibraries.add(
-            "FastAdapter",
-            "https://github.com/mikepenz/fastadapter",
-            "The bullet proof, fast and easy to use adapter library, which minimizes developing time to a fraction..."
-        )
-        OssLibraries.add(
-            "OsmDroid",
-            "https://github.com/osmdroid/osmdroid",
-            "OpenStreetMap-Tools for Android"
-        )
-        OssLibraries.add(
-            "Balloon",
-            "https://github.com/skydoves/Balloon",
-            "A lightweight popup like tooltips, fully customizable with arrow and animations."
-        )
-        OssLibraries.add(
-            "Google Play Location Services",
-            "https://developers.google.com/android/",
-            "Google Play Services Location client for Android.",
-            license = OssLicenses.custom(
-                license = "Custom Google License",
-                location = "https://developer.android.com/distribute/play-services"
-            )
-        )
-    }
-
     override fun getSystemService(name: String): Any? {
-        val service = PYDroid.getSystemService(name)
-        if (service != null) {
-            return service
-        }
-
-        return if (name == FridgeComponent::class.java.name) component else {
-            getServiceFromComponent(name) ?: super.getSystemService(name)
-        }
+        return PYDroid.getSystemService(name) ?: fallbackGetSystemService(name)
     }
 
     @CheckResult
-    private fun getServiceFromComponent(name: String): Any? {
-        val dependency = provideModuleDependencies(name)
-        if (dependency != null) {
-            return dependency
+    private fun fallbackGetSystemService(name: String): Any? {
+        return if (name == FridgeComponent::class.java.name) component else {
+            provideModuleDependencies(name) ?: super.getSystemService(name)
         }
-
-        return null
     }
 
     @CheckResult
@@ -171,5 +117,55 @@ class FridgeFriend : Application() {
                 else -> null
             }
         }
+    }
+
+    companion object {
+
+        @JvmStatic
+        private fun addLibraries() {
+            // We are using pydroid-notify
+            OssLibraries.usingNotify = true
+
+            OssLibraries.add(
+                "Room",
+                "https://android.googlesource.com/platform/frameworks/support/+/androidx-master-dev/room/",
+                "The AndroidX Jetpack Room library. Fluent SQLite database access."
+            )
+            OssLibraries.add(
+                "WorkManager",
+                "https://android.googlesource.com/platform/frameworks/support/+/androidx-master-dev/work/",
+                "The AndroidX Jetpack WorkManager library. Schedule periodic work in a device friendly way."
+            )
+            OssLibraries.add(
+                "Dagger",
+                "https://github.com/google/dagger",
+                "A fast dependency injector for Android and Java."
+            )
+            OssLibraries.add(
+                "FastAdapter",
+                "https://github.com/mikepenz/fastadapter",
+                "The bullet proof, fast and easy to use adapter library, which minimizes developing time to a fraction..."
+            )
+            OssLibraries.add(
+                "OsmDroid",
+                "https://github.com/osmdroid/osmdroid",
+                "OpenStreetMap-Tools for Android"
+            )
+            OssLibraries.add(
+                "Balloon",
+                "https://github.com/skydoves/Balloon",
+                "A lightweight popup like tooltips, fully customizable with arrow and animations."
+            )
+            OssLibraries.add(
+                "Google Play Location Services",
+                "https://developers.google.com/android/",
+                "Google Play Services Location client for Android.",
+                license = OssLicenses.custom(
+                    license = "Custom Google License",
+                    location = "https://developer.android.com/distribute/play-services"
+                )
+            )
+        }
+
     }
 }
